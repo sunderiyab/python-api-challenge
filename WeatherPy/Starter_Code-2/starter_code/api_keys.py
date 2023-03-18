c3f9ed3f62ecb77674fa23641f0d2bd9928524a8
sunderiyab <hairiima888@gmail.com>
2023-03-17 22:51:07 -0700

    added api key

--- WeatherPy/Starter_Code-2/starter_code/api_keys.py
+++ WeatherPy/Starter_Code-2/starter_code/api_keys.py
@@ -1,5 +1,7 @@
+
+
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "2d4ca65f86d43a4e7134189dad7157c4"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "451bc19234f849c8a9841e36b09924fb"
